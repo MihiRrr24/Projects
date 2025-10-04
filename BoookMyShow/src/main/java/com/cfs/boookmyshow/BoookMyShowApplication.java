73d60b6228d6aed6e2af1b46be246486724462c0
MihiRrr24 <mihirshrestha4@gmail.com>
2025-10-04 09:37:09 +0530

    IDE Files

---
+++ BoookMyShow/src/main/java/com/cfs/boookmyshow/BoookMyShowApplication.java
@@ -0,0 +1,13 @@
+package com.cfs.boookmyshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoookMyShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoookMyShowApplication.class, args);
+    }
+
+}
---
+++ BoookMyShow/src/test/java/com/cfs/boookmyshow/BoookMyShowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cfs.boookmyshow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoookMyShowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
